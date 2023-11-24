aab3df5a1c918cc35bca7a2d2feb788af716a5d1
Nick Lillie <nlillie@certipay.com>
2023-11-23 19:23:15 -0500

    testing

--- standingsBuilder.js
+++ standingsBuilder.js
@@ -1,9 +1,1 @@
-const CLIENT_ID = '<YOUR_CLIENT_ID>';
-const API_KEY = '<YOUR_API_KEY>';
-
-// Discovery doc URL for APIs used by the quickstart
-const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+const testerVar = "test me";
